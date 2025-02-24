c71b27feaa1898184f3e9790813cb46048a2eb83
Gabriel Matias Sosa <sosagabriel79@gmail.com>
2025-02-24 00:00:11 -0300

    feat: se agrega endpoints con json

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,66 @@
  const char* password = "3B779BD0C0";
  ESP8266WebServer server(80);
   
+// Define a class to represent the JSON data
+class MyData {
+  public:
+      String name;
+      int age;
+  
+      MyData(const String& name, int age) : name(name), age(age) {}
+  };
+  
+  // Handle POST request with JSON body
+  void handlePostRequestii() {
+      if (server.hasArg("plain") == false) { // Check if body received
+          server.send(400, "text/plain", "Body not received");
+          return;
+      }
+      String body = server.arg("plain");
+  
+      // Parse JSON
+      StaticJsonDocument<200> doc;
+      DeserializationError error = deserializeJson(doc, body);
+      if (error) {
+          server.send(400, "application/json", "{\"error\":\"Invalid JSON\"}");
+          return;
+      }
+  
+      // Extract data from JSON
+      String name = doc["name"];
+      int age = doc["age"];
+  
+      // Create an instance of MyData
+      MyData data(name, age);
+  
+      // Process the data as needed
+      // For example, you can print it to the serial monitor
+      Serial.print("Name: ");
+      Serial.println(data.name);
+      Serial.print("Age: ");
+      Serial.println(data.age);
+  
+      // Send response
+      server.send(200, "application/json", "{\"message\":\"POST request received\", \"name\":\"" + data.name + "\", \"age\":" + String(data.age) + "}");
+  }
+  
+
+
  // Serving Hello world
  void getHelloWord() {
      server.send(200, "text/json", "{\"name\": \"Hello world\"}");
  }
   
+ void handlePostRequest() {
+  if (server.hasArg("plain") == false) { // Check if body received
+      server.send(400, "text/plain", "Body not received");
+      return;
+  }
+  String body = server.arg("plain");
+  // Process the body as needed
+  server.send(200, "application/json", "{\"message\":\"POST request received\", \"body\":\"" + body + "\"}");
+}
+
  void handleAuthorizedRequest() {
   if (!server.hasHeader("Authorization")) {
     server.send(401, "text/plain", "Unauthorized");
@@ -47,6 +102,8 @@
      });
      server.on(F("/helloWorld"), HTTP_GET, getHelloWord);
      server.on("/secure", HTTP_GET, handleAuthorizedRequest);
+     server.on("/postEndpoint", HTTP_POST, handlePostRequest); // Declare POST handler
+     server.on("/postEndpointii", HTTP_POST, handlePostRequestii); 
  }
   
  // Manage not found URL
